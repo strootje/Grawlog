4a75d5d8b352638f40af41b55ee7c7c27fac225b
Bas Stroosnijder <bas@strootje.com>
2017-10-12 22:27:20 +0200

    total conan makeover and travis / appveyer initial config

---
+++ conanfile.py
@@ -0,0 +1,42 @@
+from conans import ConanFile, CMake
+
+class GrawlogConan(ConanFile):
+	name = "Grawlog"
+	version = "0.1"
+
+	license = "MPL-2.0"
+	description = "Sane Logging Library for cpp"
+	url = "https://github.com/strootje/grawlog"
+
+	settings = "os", "compiler", "build_type", "arch"
+	options = { "shared": [True, False], "build_docs": [True, False], "build_specs": [True, False], "build_tests": [True, False]}
+	default_options = "shared=False", "build_docs=False", "build_specs=False", "build_tests=False"
+
+	generators = "cmake"
+	exports_sources = "*"
+
+	requires = "gtest/1.8.0@conan/stable"
+
+	def configure(self):
+		self.options["gtest"].shared = self.options.shared
+
+	def build(self):
+		cmake = CMake(self)
+		
+		cmake.definitions["BUILD_SHARED_LIBS"] = True if self.options["shared"] == True else False
+		cmake.definitions["BUILD_DOCS"] = 1 if self.options["build_docs"] == True else 0
+		cmake.definitions["BUILD_SPECS"] = 1 if self.options["build_specs"] == True else 0
+		cmake.definitions["BUILD_TESTS"] = 1 if self.options["build_tests"] == True else 0
+		cmake.configure()
+		cmake.build()
+
+	def package(self):
+		self.copy("*.hpp", dst="include/Grawlog", src="include")
+		self.copy("*.lib", dst="lib", keep_path=False)
+		self.copy("*.so", dst="lib", keep_path=False)
+		self.copy("*.dylib", dst="lib", keep_path=False)
+		self.copy("*.dll", dst="bin", keep_path=False)
+		self.copy("*.a", dst="lib", keep_path=False)
+
+	def package_info(self):
+		self.cpp_info.libs = ["Grawlog"]
---
+++ tests/LoggerTests/Program.cpp
@@ -0,0 +1,7 @@
+#include "./Logger.hpp"
+#include <gtest/gtest.h>
+
+TEST(LoggerTests, DoesOneEqOne)
+{
+	ASSERT_EQ(1, 1);
+}
