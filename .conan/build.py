aba298e24abb7bf7e4eccf511ea4a6b759572e57
Bas Stroosnijder <bas@strootje.com>
2017-10-22 07:11:09 +0200

    cmake revert

--- .conan/build.py
+++ .conan/build.py
@@ -1,6 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args = "--build missing -tf tests/PackageTests -s compiler.libcxx=libstdc++17")
+    builder = ConanMultiPackager(args = "--build missing -tf tests/PackageTests")
     builder.add_common_builds()
     builder.run()
