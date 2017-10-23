3cf211ac8f78577a64fc7883c6def541bae3470f
Bas Stroosnijder <bas@strootje.com>
2017-10-23 19:30:01 +0200

    enabled unit tests in ci build

--- .conan/build.py
+++ .conan/build.py
@@ -1,6 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args = "--build missing -tf tests/PackageTests")
+    builder = ConanMultiPackager(args = "--build missing -tf tests/PackageTests -o build_tests=True")
     builder.add_common_builds()
     builder.run()
