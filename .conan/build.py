cbc417358613519e6ba0fb141f7585f47bd7d1c5
Bas Stroosnijder <bas@strootje.com>
2017-10-27 07:41:28 +0200

    cmake 3.9 and c++14

--- .conan/build.py
+++ .conan/build.py
@@ -3,16 +3,5 @@ import platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(args = "-tf ./tests/PackageTests --build missing")
-    
-    if platform.system() == "Windows":
-        builder.add(settings={"os": "Windows", "arch": "x86", "build_type": "Debug", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MTd"}, options={}, env_vars={}, build_requires={})
-        builder.add(settings={"os": "Windows", "arch": "x86_64", "build_type": "Debug", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MTd"}, options={}, env_vars={}, build_requires={})
-        builder.add(settings={"os": "Windows", "arch": "x86", "build_type": "Release", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MT"}, options={}, env_vars={}, build_requires={})
-        builder.add(settings={"os": "Windows", "arch": "x86_64", "build_type": "Release", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MT"}, options={}, env_vars={}, build_requires={})
-    else:
-        builder.add(settings={"arch": "x86", "build_type": "Debug", "compiler": "clang"}, options={}, env_vars={}, build_requires={})
-        builder.add(settings={"arch": "x86_64", "build_type": "Debug", "compiler": "clang"}, options={}, env_vars={}, build_requires={})
-        builder.add(settings={"arch": "x86", "build_type": "Release", "compiler": "clang"}, options={}, env_vars={}, build_requires={})
-        builder.add(settings={"arch": "x86_64", "build_type": "Release", "compiler": "clang"}, options={}, env_vars={}, build_requires={})
-
+    builder.add_common_builds()
     builder.run()
