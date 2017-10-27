e1f0c19ad70239419bc4fe7235e2f2922db93a1e
Bas Stroosnijder <bas@strootje.com>
2017-10-27 07:28:16 +0200

    build py

--- .conan/build.py
+++ .conan/build.py
@@ -3,13 +3,15 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(args = "-tf ./tests/PackageTests --build missing")
     
-    builder.add(settings={"arch": "x86", "build_type": "Debug"},
-                options={}, env_vars={}, build_requires={})
-    builder.add(settings={"arch": "x86_64", "build_type": "Debug"},
-                options={}, env_vars={}, build_requires={})
-    builder.add(settings={"arch": "x86", "build_type": "Release"},
-                options={}, env_vars={}, build_requires={})
-    builder.add(settings={"arch": "x86_64", "build_type": "Release"},
-                options={}, env_vars={}, build_requires={})
+    if platform.system() == "Windows":
+        builder.add(settings={"arch": "x86", "build_type": "Debug", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MTd"}, options={}, env_vars={}, build_requires={})
+        builder.add(settings={"arch": "x86_64", "build_type": "Debug", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MTd"}, options={}, env_vars={}, build_requires={})
+        builder.add(settings={"arch": "x86", "build_type": "Release", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MT"}, options={}, env_vars={}, build_requires={})
+        builder.add(settings={"arch": "x86_64", "build_type": "Release", "compiler": "Visual Studio", "compiler.version": 14, "compiler.runtime": "MT"}, options={}, env_vars={}, build_requires={})
+    else:
+        builder.add(settings={"arch": "x86", "build_type": "Debug"}, options={}, env_vars={}, build_requires={})
+        builder.add(settings={"arch": "x86_64", "build_type": "Debug"}, options={}, env_vars={}, build_requires={})
+        builder.add(settings={"arch": "x86", "build_type": "Release"}, options={}, env_vars={}, build_requires={})
+        builder.add(settings={"arch": "x86_64", "build_type": "Release"}, options={}, env_vars={}, build_requires={})
 
     builder.run()
